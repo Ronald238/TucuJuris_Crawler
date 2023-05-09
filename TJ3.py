2c58d0a1d343d833cb501b2089cb8c46b2da3bd1
Ronald Mickael <127986881+Ronald238@users.noreply.github.com>
2023-05-09 15:57:34 -0300

    Update TJ3.py

--- TJ3.py
+++ TJ3.py
@@ -1,10 +1,10 @@
-from TJ2 import ConsultaProcessual
+from TJ2 import ProceduralConsultation
 
-class TjAP_Tucujuris(ConsultaProcessual):
+class TjAP_Tucujuris(ProceduralConsultation):
     
-    def __init__(self, instancia='1'):
-        self.UrlBase = 'http://tucujuris.tjap.jus.br/'
-        self.UrlBusca = self.UrlBase + 'api/publico/buscar-autos-consulta-publica'       
-        self.Tribunal = 'TjAP'
-        self.Origem = 'tjap - tucujuris'
-        self.Instancia = instancia
+     def __init__(self, instance='1'):
+         self.UrlBase = 'http://tucujuris.tjap.jus.br/'
+         self.UrlBusca = self.UrlBase + 'api/publico/buscar-autos-consulta-publica'
+         self.Court = 'TjAP'
+         self.Origem = 'tjap - tucujuris'
+         self.Instance = instance
